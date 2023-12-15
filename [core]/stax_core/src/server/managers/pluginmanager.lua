f0728275ed5f40b2ca954acf57dfcd4683826570
Xander Harrison <xander.harrison77@gmail.com>
2023-12-15 05:11:04 -0500

    PluginManager logic

--- [core]/stax_core/src/server/managers/pluginmanager.lua
+++ [core]/stax_core/src/server/managers/pluginmanager.lua
@@ -1,29 +1,72 @@
 ---@type StaxLogger
-local Logger = Stax.Component.Fetch("Logger")
+local Logger
 
 ---@type StaxPlugin
-local Plugin = Stax.Component.Fetch("Plugin")
+local Plugin
 
+---@class StaxPluginManager
+---@field public PLUGINS { [string]: StaxPlugin }
 local PluginManager = {
-  PLUGIN = {}
+  PLUGINS = {}
 }
 
-function PluginManager.Add()
-
+--- Adds a new plugin to the plugin manager
+---@param plugin StaxPlugin
+function PluginManager.Add(plugin)
+  PluginManager.PLUGINS[plugin.Data.Key] = plugin
+  TriggerEvent("Stax::Server::PluginAdded", plugin)
 end
 
-function PluginManager.Remove()
-
+--- Adds a new plugin to the plugin manager
+---@param plugin StaxPlugin
+function PluginManager.Remove(plugin)
+  PluginManager.PLUGINS[plugin.Data.Key] = nil
 end
 
-function PluginManager.Fetch()
+--- Fetches a plugin by its key
+---@param key string
+---@return (StaxPlugin | nil)
+function PluginManager.FetchFromKey(key)
+  if PluginManager.PLUGINS[key] then
+    return PluginManager.PLUGINS[key]
+  end
 
+  return nil
 end
 
---- Creates an event for when the resource starts
+--- Fetches a plugin by its resource name
 ---@param resource string
-function PluginManager.OnResourceStart(resource)
-  local plugin = Plugin.Register(resource)
+---@return (StaxPlugin | nil)
+function PluginManager.FetchFromResource(resource)
+  for _, plugin in pairs(PluginManager.PLUGINS) do
+    if plugin.Resource == resource then
+      return plugin
+    end
+  end
+
+  return nil
 end
 
-AddEventHandler("onResourceStart", PluginManager.OnResourceStart)
+--- Event to handle resource starting
+---@param resource string
+function PluginManager.StaxReady(resource)
+  if GetCurrentResourceName() == resource then
+    Plugin = Stax.Require("Plugin")
+    Logger = Stax.Require("Logger")
+  end
+
+  local plugin = Plugin.Create(resource)
+
+  if plugin then
+    PluginManager.Add(plugin)
+  end
+end
+
+--- Event to handle plugin being added to manager
+---@param plugin StaxPlugin
+function PluginManager.OnPluginAdded(plugin)
+  Logger.Success("PluginManagaer.OnPluginAdded", tostring(plugin.Data.Name .. " ADDED"))
+end
+
+AddEventHandler("Stax::Shared::Ready", PluginManager.StaxReady)
+AddEventHandler("Stax::Server::PluginAdded", PluginManager.OnPluginAdded)
