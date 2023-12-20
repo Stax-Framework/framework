586476206bce07d4b0b5804c1e8c26d00e8842f4
Xander Harrison <xander.harrison77@gmail.com>
2023-12-20 02:30:49 -0500

    Added config and locale loading logic

--- [core]/stax_core/libs/stax.lua
+++ [core]/stax_core/libs/stax.lua
@@ -1,3 +1,157 @@
+---@class StaxConfig
+---@field STORAGE { [string]: table }
+Config = {
+  STORAGE = {}
+}
+Config.__index = Config
+
+function Config.Init(configs)
+  local newConfig = {
+    STORAGE = {}
+  }
+
+  if configs["server"] then
+    for k, v in pairs(configs["server"]) do
+      newConfig.STORAGE[k] = v
+    end
+  end
+
+  if configs["client"] then
+    for k, v in pairs(configs["client"]) do
+      newConfig.STORAGE[k] = v
+    end
+  end
+  
+  if configs["shared"] then
+    for k, v in pairs(configs["shared"]) do
+      newConfig.STORAGE[k] = v
+    end
+  end
+
+  for k, v in pairs(configs) do
+    if k ~= "shared" and k ~= "client" and k ~= "server" then
+      newConfig.STORAGE[k] = v
+    end
+  end
+
+  setmetatable(newConfig, Config)
+
+  return newConfig
+end
+
+function Config:Fetch(path)
+  --- Splits string into a table based on a seperator
+  ---@param passed string
+  ---@param sep string
+  ---@return table
+  local function split(passed, sep)
+    if sep == nil then
+      sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(passed, "([^"..sep.."]+)") do
+      table.insert(t, str)
+    end
+    return t
+  end
+
+  local function loadStepper(currentData, nextIndex)
+    return currentData[nextIndex]
+  end
+
+  local result = {}
+
+  if not path then
+    return self.STORAGE
+  end
+
+  path = split(path, ".")
+
+  if type(path) == "table" then
+    local lastStep
+
+    for _, v in pairs(path) do
+      local currentStep = loadStepper(self.STORAGE, v)
+
+      if currentStep == nil then
+        return lastStep[v]
+      else
+        lastStep = currentStep
+      end
+    end
+
+    result = lastStep
+  end
+
+  return result
+end
+
+---@class StaxLocale
+---@field STORAGE { [string]: table }
+Locale = {
+  STORAGE = {}
+}
+Locale.__index = Locale
+
+function Locale.Init(locales)
+  local newLocale = { STORAGE = locales }
+
+  for k, v in pairs(locales) do
+    newLocale.STORAGE[k] = v
+  end
+
+  setmetatable(newLocale, Config)
+
+  return newLocale
+end
+
+function Locale:Fetch(path)
+  --- Splits string into a table based on a seperator
+  ---@param passed string
+  ---@param sep string
+  ---@return table
+  local function split(passed, sep)
+    if sep == nil then
+      sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(passed, "([^"..sep.."]+)") do
+      table.insert(t, str)
+    end
+    return t
+  end
+
+  local function loadStepper(currentData, nextIndex)
+    return currentData[nextIndex]
+  end
+
+  local result = {}
+
+  if not path then
+    return self.STORAGE
+  end
+
+  path = split(path, ".")
+
+  if type(path) == "table" then
+    local lastStep
+
+    for _, v in pairs(path) do
+      local currentStep = loadStepper(self.STORAGE, v)
+
+      if currentStep == nil then
+        return lastStep[v]
+      else
+        lastStep = currentStep
+      end
+    end
+
+    result = lastStep
+  end
+
+  return result
+end
+
 ---@class ComponentDetails
 ---@field NAME string Components Name
 ---@field REQUIREMENTS string[]
@@ -72,20 +226,57 @@ end
 
 ---@class Stax
 ---@field _RegisteredComponents { [string]: table }
+---@field _Resource string
 Stax = {
   --- Variables
   _RegisteredComponents = {},
   _LoadedComponents = {},
   _LastRegisteredComponent = nil,
+  _Resource = GetCurrentResourceName(),
+  _IsServer = IsDuplicityVersion(),
 
   --- Internal Components
-  Component = Component
+  Component = Component,
+  Config = Config,
+  Locale = Locale
 }
-Stax.__index = Stax
 
-function Stax.Init()
+function Stax.Init(self)
+  local _isServer = IsDuplicityVersion()
+
   print("____________________ STAX ____________________")
   print("[" .. GetCurrentResourceName() .. "]: INITIALIZING...")
+
+  ---@param resource string
+  local function HandleResourceStart(resource)
+    if Stax._Resource ~= resource then return end
+
+    if _isServer then
+      TriggerEvent("Stax::Server::ResourceStarted", resource)
+    else
+      TriggerServerEvent("Stax::Client::ResourceStarted", resource)
+    end
+
+    Stax.LoadConfig(self)
+    Stax.LoadLocale(self)
+  end
+
+
+  if _isServer then
+    AddEventHandler("onServerResourceStart", HandleResourceStart)
+  else
+    AddEventHandler("onClientResourceStart", HandleResourceStart)
+  end
+end
+
+---@return boolean
+function Stax.IsServer()
+  return IsDuplicityVersion() == true
+end
+
+---@return boolean
+function Stax.IsClient()
+  return IsDuplicityVersion() == false
 end
 
 --- Returns a component from component name
@@ -104,17 +295,43 @@ function Stax.Register(component, requirements)
   return Stax.Component.Register(component, requirements)
 end
 
-function Stax.LoadConfig()
+function Stax.LoadConfig(self)
+  local event
 
+  local function _load(configs)
+    Stax.Config = Config.Init(configs)
+
+    if event then
+      RemoveEventHandler(event)
+    end
+  end
+
+  if Stax.IsServer() then
+    event = AddEventHandler("Stax::Shared::LoadConfigs", _load)
+  else
+    event = RegisterNetEvent("Stax::Shared::LoadConfigs", _load)
+  end
 end
 
-function Stax.LoadLocale()
+function Stax.LoadLocale(self)
+  local event
 
+  local function _load(locales)
+    Stax.Locale = Locale.Init(locales)
+
+    if event then
+      RemoveEventHandler(event)
+    end
+  end
+
+  if Stax.IsServer() then
+    event = AddEventHandler("Stax::Shared::LoadLocales", _load)
+  else
+    event = RegisterNetEvent("Stax::Shared::LoadLocales", _load)
+  end
 end
 
 Citizen.CreateThread(function()
-  Stax.Init()
-
   local function loaded()
     if not Stax._LastRegisteredComponent then
       Stax._LastRegisteredComponent = os.time()
@@ -134,3 +351,5 @@ Citizen.CreateThread(function()
 
   TriggerEvent("Stax::Shared::Ready", GetCurrentResourceName())
 end)
+
+Stax.Init(Stax)
