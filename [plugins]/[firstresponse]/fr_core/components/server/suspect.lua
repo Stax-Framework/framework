1edfe31d60bf6047b979d292e3856e3a7c22c343
Xander Harrison <xander.harrison77@gmail.com>
2024-01-31 22:35:11 -0500

    Suspect Class

---
+++ [plugins]/[firstresponse]/fr_core/components/server/suspect.lua
@@ -0,0 +1,39 @@
+---@class FRSuspect
+---@field COMPONENT StaxComponent
+local Suspect = {
+  COMPONENT = Stax.Component.Init("FRSuspect")
+}
+
+function Suspect.Init()
+
+end
+
+function Suspect.Inspect()
+
+end
+
+function Suspect.SetInspect()
+
+end
+
+function Suspect.Info()
+
+end
+
+function Suspect.SetInfo()
+
+end
+
+function Suspect.Cuff(active)
+
+end
+
+function Suspect.Injuries()
+
+end
+
+function Suspect.SetInjuries()
+
+end
+
+Stax.Register(Suspect)
