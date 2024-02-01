28c871a42eb173156d3dc9c955875d3570949a09
Xander Harrison <xander.harrison77@gmail.com>
2024-01-31 22:25:39 -0500

    Initial External Component Loading

---
+++ [plugins]/[firstresponse]/fr_core/components/server/managers/unitmanager.lua
@@ -0,0 +1,23 @@
+---@class FRUnitManager
+---@field COMPONENT StaxComponent
+local UnitManager = {
+  COMPONENT = Stax.Component.Init("FRUnitManager")
+}
+
+function UnitManager.Add()
+
+end
+
+function UnitManager.Remove()
+
+end
+
+function UnitManager.Fetch()
+
+end
+
+function UnitManager.Update()
+
+end
+
+Stax.Register(UnitManager)
---
+++ [plugins]/[firstresponse]/fr_core/components/server/unit.lua
@@ -0,0 +1,47 @@
+---@class FRUnit
+---@field COMPONENT StaxComponent
+local Unit = {
+  COMPONENT = Stax.Component.Init("FRUnit")
+}
+
+function Unit.Init()
+
+end
+
+function Unit.SetDepartment()
+
+end
+
+function Unit.GetDepartment()
+
+end
+
+function Unit.SetDepartmentRank()
+
+end
+
+function Unit.GetDepartmentRank()
+
+end
+
+function Unit.SetActive(active)
+
+end
+
+function Unit.GetActive()
+
+end
+
+function Unit.SetVehicle()
+
+end
+
+function Unit.SetStatus()
+
+end
+
+function Unit.GetStatus()
+
+end
+
+Stax.Register(Unit)
