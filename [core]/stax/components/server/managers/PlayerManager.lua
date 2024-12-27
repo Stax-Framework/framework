6eb4c291c68d362fe560270efe96208ce288d2ec
xander1998 <xander.harrison77@gmail.com>
2024-12-27 14:26:19 -0500

    Playermanager updated to new syntax

---
+++ [core]/stax/components/server/managers/PlayerManager.lua
@@ -0,0 +1,24 @@
+---@class PlayerManager
+local PlayerManager = {
+    COMPONENT = Stax.Component.Init("PlayerManager")
+}
+
+--- Adds a new player to the PlayerManager instance
+---@param player Player
+function PlayerManager.Add(player)
+    return exports.stax:PlayerManager_Add(player)
+end
+
+--- Removes a player from the PlayerManager instance
+---@param playerId string | number
+function PlayerManager.Remove(playerId)
+    return exports.stax:PlayerManager_Remove(playerId)
+end
+
+--- Fetches a player from the PlayerManager instance
+---@param playerId string | number
+function PlayerManager.Fetch(playerId)
+    return exports.stax:PlayerManager_Fetch(playerId)
+end
+
+Stax.Component.Register(PlayerManager)
--- [core]/stax/source/server/managers/PlayerManager.lua
+++ [core]/stax/source/server/managers/PlayerManager.lua
@@ -1,15 +1,53 @@
+---@type Logger
+local _Logger
+
 local Manager = {
     Players = {}
 }
 
+---@param player Player
 local function _add(player)
+    if Manager.Players[player.source] then
+        _Logger.Error("PlayerManager", "Couldn't add player [" .. player.source .. "] | " .. player.name .. " because they already exist!")
+        return
+    end
+
     table.insert(Manager.Players, player)
+
+    _Logger.Success("PlayerManager", "Added player [" .. player.source .. "] | " .. player.name .. " to the manager!")
 end
 
-local function _remove(playerSource)
-    table.remove(Manager.Players, playerSource)
+local function _remove(playerId)
+    if not Manager.Players[playerId] then
+        _Logger.Error("PlayerManager", "Couldn't add player [" .. playerId .. "] | because they don't exist in the manager!")
+        return
+    end
+
+    table.remove(Manager.Players, playerId)
 end
 
-local function _fetch()
+local function _fetch(playerId)
+    local player = Manager.Players[playerId]
 
+    if not player then
+        _Logger.Error("PlayerManager", "Couldn't add player [" .. playerId .. "] | because they don't exist in the manager!")
+        return nil
+    end
+
+    return player
 end
+
+---[[
+--- LOADER EVENT
+---]]
+Stax.Event("ComponentLoader", "Ready").create(false, function()
+    ---@type Logger
+    _Logger = Stax.Component.FetchAsync("Logger")
+end)
+
+---[[
+--- EXPORTS
+---]]
+Stax.Export("PlayerMangaer_Add", _add)
+Stax.Export("PlayerManger_Remove", _remove)
+Stax.Export("PlayerManager_Fetch", _fetch)
