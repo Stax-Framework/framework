39a586d0d29ee055eca6cf9b3164a2eaff24ee77
xander1998 <xander.harrison77@gmail.com>
2025-01-01 11:03:12 -0500

    Starting to handle actual connection logic

--- [core]/stax/source/server/managers/ConnectionManager.lua
+++ [core]/stax/source/server/managers/ConnectionManager.lua
@@ -1,9 +1,17 @@
+---[[
+--- COMPONENTS
+---]]
 ---@type Logger
 local _Logger
 ---@type PlayerManager
 local _PlayerManager
 ---@type StaxPlayer
 local _Player
+
+---[[
+--- CONFIGS
+---]]
+local ServerWhitelisted = Stax.FetchConfig("Server.Whitelisted")
 
 local _ConnectingStates = {
     ["connecting"] = 1,
@@ -16,13 +24,19 @@ local Manager = {
     QueuedPlayers = {}
 }
 
-local function _connecting(playerName, setKickReason, deferrals)
+local function _connecting(source, playerName, setKickReason, deferrals)
     Citizen.Wait(1)
 
-    local src = tostring(source)
-    local playerInstance = _Player.new(src)
+    local playerInstance = _Player.new(source)
 
     deferrals.update("Initializing Player Instance!")
+
+    Citizen.Wait(500)
+
+    if not playerInstance then
+        deferrals.done("Couldn't initialize player instance!")
+        return
+    end
 
     if playerInstance then
         deferrals.update("Player Instance Initialized!")
@@ -30,12 +44,35 @@ local function _connecting(playerName, setKickReason, deferrals)
         deferrals.done("Couldn't initialize player instance!")
     end
 
-    -- TODO: ADD CONNECTION LOGIC CHECKS
+    Citizen.Wait(500)
+
+    -- TODO: ADD WHITELIST LOGIC CHECKS
+    if ServerWhitelisted then
+        deferrals.update("Server is whitelisted... Checking your whitelist status!")
+
+        Citizen.Wait(500)
+
+        if playerInstance.whitelisted then
+            deferrals.update("Whitelist check passed!")
+        else
+            deferrals.done("You are not whitelisted to play on the server :(")
+        end
+    end
+
+
+    -- TODO: ADD BAN CHECK LOGIC
+    deferrals.update("Checking Queue System... Logic doesn't exist yet.. Passing..")
+
+    Citizen.Wait(500)
+
     -- TODO: ADD QUEUE CONNECTION SYSTEM
+    deferrals.update("Checking Queue System... Logic doesn't exist yet.. Passing..")
+
+    Citizen.Wait(500)
 
     print(playerName .. " is connecting to the server!")
 
-    deferrals.done("Stopping you from joining here...")
+    deferrals.done("You can join the server but nah still deving XD")
 end
 
 ---@param newSource string
@@ -44,8 +81,6 @@ local function _joined(newSource, oldSource)
     local src = newSource
 
     print(GetPlayerName(src) .. " is joining the server!")
-
-
 end
 
 local function _loadComponents()
@@ -57,6 +92,11 @@ end
 ---[[
 --- EVENTS
 ---]]
-Stax.Event("playerConnecting").create(false, _connecting)
+Stax.Event("playerConnecting").create(false, function(playerName, setKickReason, deferrals)
+    local src = source
+
+    _connecting(src, playerName, setKickReason, deferrals)
+end)
+
 Stax.Event("playerJoining").create(false, _joined)
 Stax.Event("ComponentLoader", "Ready").create(false, _loadComponents)
