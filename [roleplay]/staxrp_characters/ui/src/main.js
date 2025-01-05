c0e9879665a167381eb92c07b8f6fc206636c238
xander1998 <xander.harrison77@gmail.com>
2025-01-04 19:52:18 -0500

    Removing UI for now. Not sure what I want to do yet

--- [roleplay]/staxrp_characters/ui/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-
-const pinia = createPinia()
-
-createApp(App).use(router).use(pinia).mount('#app')
